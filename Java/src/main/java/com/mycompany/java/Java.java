aa72de0317868023341a2d991b7ef2e611308a1c
Rafa3322 <rafa33223322@gmail.com>
2024-02-17 02:40:03 -0300

    Rafael Deus33

--- Java/src/main/java/com/mycompany/java/Java.java
+++ Java/src/main/java/com/mycompany/java/Java.java
@@ -13,5 +13,7 @@ public class Java {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Sexo oje");
+        System.out.println("Sou foda mano");
+        
     }
 }
